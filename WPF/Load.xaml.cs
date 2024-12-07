6734ddf90e1a6292dce2e3660ef95237c59b3ffd
𝑵𝒊𝒌𝒊𝒕𝒂 𝑩𝒖𝒚𝒂𝒏𝒐𝒗🧸 <hlebushek0612@ya.ru>
2024-12-08 04:34:38 +0900

    v0.0.8

---
+++ WPF/Load.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            StartSystem();
+        }
+
+        private DispatcherTimer timer;
+        private int progressTimer = 0;
+
+        private void StartSystem()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void LoadData()
+        {
+
+        }
+    }
+}
